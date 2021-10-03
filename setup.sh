a349c218d67e9e185c5c6377052cb79b532d7bc3
4-o-4 <yuzhakov.lev@gmail.com>
2021-10-03 22:07:35 +0300

    feat: add wordpress

--- setup.sh
+++ setup.sh
@@ -7,8 +7,10 @@ docker pull metallb/speaker:v0.8.2
 docker pull metallb/controller:v0.8.2
 
 docker build -t docker_img_nginx ./srcs/nginx
+docker build -t docker_img_wordpress ./srcs/wordpress
 
 kubectl apply -f ./srcs/nginx/nginx.yaml
+kubectl apply -f ./srcs/wordpress/wordpress.yaml
 
 minikube addons enable metallb
 
